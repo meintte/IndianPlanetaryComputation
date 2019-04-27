23f95b390ee2d4cf8b8dbdaa59f80c5019484d16
Daniel Stalder <dstalder@student.ethz.ch>
2019-04-27 21:19:51 +0200

    moved the whole thing in a function

--- main.py
+++ main.py
@@ -28,7 +28,17 @@ class AngleAndSinHandler:
     def getPositveAngle(self, decimalAngle):
         while decimalAngle < 0:
             decimalAngle += 360
+        while decimalAngle > 360:
+            decimalAngle -= 360
         return decimalAngle
+
+    def roundToMinutes(self, decimalAngle):
+        _deg, _min, _sec = self.DecimalDegreeToIndividualAngleUnits(decimalAngle)
+        angle = self.IndividualAngleUnitsToDecimalDegree(_deg, _min, 0)
+        if (_sec >= 30):
+            return angle+1/60.
+        else:
+            return angle
 
     # positivity is required
     def _getQuadrantOfAngle(self, decimalAngle):
@@ -68,6 +78,16 @@ class AngleAndSinHandler:
 
             print('{:20}: {}° {}\' {}\'\''.format(name, _deg, _min, _sec))
 
+    def makePositiveRoundAndPrint(self, name, angle, inDecimal=True, doRound=False):
+        # make positive
+        angle = self.getPositveAngle(angle)
+        # do the rounding
+        if doRound:
+            angle = self.roundToMinutes(angle)
+        # print the angle
+        self.printAngle(name, angle, inDecimal)
+
+        return angle
 
 # returns the Radius and the known theta Sin pairs (each as separate np.array)
 def readCsvFile(filename):
@@ -90,6 +110,10 @@ def readCsvFile(filename):
 
 def getSizeEpicycle(size_at_0, size_at_90, r_for_sin, handlerAngleSin, decimalAngle):
     return size_at_0 + (size_at_90 - size_at_0) * abs(handlerAngleSin.sinOf(decimalAngle)) / r_for_sin
+
+def getRadiusEpicycle(size_at_0, size_at_90, radiusDeferent, handlerAngleSin, decimalAngle):
+    sizeEpicycle = getSizeEpicycle(size_at_0, size_at_90, radiusDeferent, handlerAngleSin, decimalAngle)
+    return sizeEpicycle / 360. * radiusDeferent
 
 def getDecimalAngleFromRotation(revolutionSpeed, elapsedDays, period):
     numRevolutions = (revolutionSpeed * elapsedDays) / (1. * period)
@@ -120,7 +144,99 @@ def getSlowEquation(radiusSlow, radiusDeferent, handlerAngleSin, kappa):
 
     return mu
 
-##########
+#############################################################
+
+def do4stepProcedure( \
+    _yuga, _days_in_yuga, _days_since_epoch, \
+    _radiusDeferent, _handler, \
+    _meanPlanet_revolutions, _fast_apogee_revolutions, _longitude_slow_apogee, \
+    _sizeSlow_at_0, _sizeSlow_at_90, _sizeFast_at_0, _sizeFast_at_90, \
+    _doRounding, _printDecimalDegree, \
+    _firstSign, _secondSign, _thirdSign, _fourthSign ):
+
+    # 4 step procedure, from suryasiddhanta
+
+    # 0th step
+    # calculate the mean planets longitude (lambda_bar)
+    lambda_bar = getDecimalAngleFromRotation(_meanPlanet_revolutions, _days_since_epoch, _days_in_yuga)
+    lambda_bar = _handler.makePositiveRoundAndPrint('lambda_bar', lambda_bar, _printDecimalDegree, _doRounding)
+
+    ################# START 1st step #################
+    # apply half the fast equation to the mean planet
+    lambda_sigma = getDecimalAngleFromRotation(_fast_apogee_revolutions, _days_since_epoch, _days_in_yuga)
+    lambda_sigma = _handler.makePositiveRoundAndPrint('lambda_sigma', lambda_sigma, _printDecimalDegree, _doRounding)
+
+    kappa_sigma_1 = lambda_bar - lambda_sigma
+    kappa_sigma_1 = _handler.makePositiveRoundAndPrint('kappa_sigma_1', kappa_sigma_1, _printDecimalDegree, _doRounding)
+
+    # get the current radius of the epicycle
+    radiusFast = getRadiusEpicycle(_sizeFast_at_0, _sizeFast_at_90, _radiusDeferent, _handler, kappa_sigma_1)
+
+    sigma_1 = getFastEquation(radiusFast, _radiusDeferent, _handler, kappa_sigma_1)
+    sigma_1 = _handler.makePositiveRoundAndPrint('sigma_1', sigma_1, _printDecimalDegree, _doRounding)
+
+    # plus or minus? use the firstSign...
+    lambda_1 = lambda_bar + _firstSign * 0.5 * sigma_1
+    lambda_1 = _handler.makePositiveRoundAndPrint('lambda_1', lambda_1, _printDecimalDegree, _doRounding)
+
+    ################# END 1st step #################
+
+    ################# START 2nd step #################
+    # apply half the slow equation to the computed result
+    lambda_mu = _longitude_slow_apogee
+    lambda_mu = _handler.makePositiveRoundAndPrint('lambda_mu', lambda_mu, _printDecimalDegree, _doRounding)
+
+    kappa_mu_1 = lambda_1 - lambda_mu
+    kappa_mu_1 = _handler.makePositiveRoundAndPrint('kappa_mu_1', kappa_mu_1, _printDecimalDegree, _doRounding)
+
+    # get the current radius of the epicycle
+    radiusSlow = getRadiusEpicycle(_sizeSlow_at_0, _sizeSlow_at_90, _radiusDeferent, _handler, kappa_mu_1)
+
+    mu_1 = getSlowEquation(radiusSlow, _radiusDeferent, _handler, kappa_mu_1)
+    mu_1 = _handler.makePositiveRoundAndPrint('mu_1', mu_1, _printDecimalDegree, _doRounding)
+
+    # plus or minus? use the secondSign...
+    lambda_2 = lambda_1 + _secondSign * 0.5 * mu_1
+    lambda_2 = _handler.makePositiveRoundAndPrint('lambda_2', lambda_2, _printDecimalDegree, _doRounding)
+
+    ################# END 2nd step #################
+
+    ################# START 3rd step #################
+    # start form the computed result, compute the slow equation,
+    # apply it whole to the mean planet
+    kappa_mu_2 = lambda_2 - lambda_mu
+    kappa_mu_2 = _handler.makePositiveRoundAndPrint('kappa_mu_2', kappa_mu_2, _printDecimalDegree, _doRounding)
+
+    # get the current radius of the epicycle
+    radiusSlow = getRadiusEpicycle(_sizeSlow_at_0, _sizeSlow_at_90, _radiusDeferent, _handler, kappa_mu_2)
+
+    mu_2 = getSlowEquation(radiusSlow, _radiusDeferent, _handler, kappa_mu_2)
+    mu_2 = _handler.makePositiveRoundAndPrint('mu_2', mu_2, _printDecimalDegree, _doRounding)
+
+    # plus or minus? use the thridSign...
+    lambda_3 = lambda_bar + _thirdSign * mu_2
+    lambda_3 = _handler.makePositiveRoundAndPrint('lambda_3', lambda_3, _printDecimalDegree, _doRounding)
+
+    ################# END 3rd step #################
+
+    ################# START 4th step #################
+    # apply the whole fast equation to the computed result
+    kappa_sigma_2 = lambda_3 - lambda_sigma
+    kappa_sigma_2 = _handler.makePositiveRoundAndPrint('kappa_sigma_2', kappa_sigma_2, _printDecimalDegree, _doRounding)
+
+    # get the current size of the epicycle
+    radiusFast = getRadiusEpicycle(_sizeFast_at_0, _sizeFast_at_90, _radiusDeferent, _handler, kappa_sigma_2)
+
+    sigma_2 = getFastEquation(radiusFast, _radiusDeferent, _handler, kappa_sigma_2)
+    sigma_2 = _handler.makePositiveRoundAndPrint('sigma_2', sigma_2, _printDecimalDegree, _doRounding)
+
+    # plus or minus? use the fourthSign...
+    lambda_true = lambda_3 + _fourthSign * sigma_2
+    lambda_true = _handler.makePositiveRoundAndPrint('lambda_true', lambda_true, _printDecimalDegree, _doRounding)
+
+    ################# END 4th step #################
+
+#############################################################
 
 # setup the Sin tables
 # it's assumed that the sin table only gives vales for angles in [0,90 deg]
@@ -128,12 +244,12 @@ radiusDeferent, thetas, sinValues = readCsvFile('SinTables/AryabhatiyaSinTable.c
 
 handler = AngleAndSinHandler(thetas, sinValues)
 
-# evidence suggest that values are rounded to the nearest minute
+# evidence suggest that angle values are rounded to the nearest minute
 doRounding = False
 # print all steps
-printAll = True
+# printAll = False
 # print angles in decimalDegree
-printDecimalDegree = False
+printDecimalDegree = True
 
 yuga = 4320000
 days_in_yuga = 1577917828
@@ -156,118 +272,21 @@ sizeFast_at_90 = 72
 
 ### END OF PARAMETER DECLARATION
 
-# 4 step procedure, from suryasiddhanta
-
-# 0th step
-# calculate the mean planets longitude (lambda_bar)
-lambda_bar = getDecimalAngleFromRotation(meanPlanet_revolutions, days_since_epoch, days_in_yuga)
-handler.printAngle('lambda_bar', lambda_bar, printDecimalDegree)
-
-################# START 1st step #################
-# apply half the fast equation to the mean planet
-lambda_sigma = getDecimalAngleFromRotation(fast_apogee_revolutions, days_since_epoch, days_in_yuga)
-handler.printAngle('lambda_sigma', lambda_sigma, printDecimalDegree)
-
-kappa_sigma_1 = lambda_bar - lambda_sigma
-handler.printAngle('kappa_sigma_1', kappa_sigma_1, printDecimalDegree)
-kappa_sigma_1 = handler.getPositveAngle(kappa_sigma_1)
-handler.printAngle('kappa_sigma_1', kappa_sigma_1, printDecimalDegree)
-
-# get the current size of the epicycle
-sizeFast = getSizeEpicycle(sizeFast_at_0, sizeFast_at_90, radiusDeferent, handler, kappa_sigma_1)
-#print('sizeFast: \t' + str(sizeFast))
-# convert the size to the radius
-radiusFast = sizeFast / 360. * radiusDeferent
-#print('radiusFast: \t' + str(radiusFast))
-
-sigma_1 = getFastEquation(radiusFast, radiusDeferent, handler, kappa_sigma_1)
-handler.printAngle('sigma_1', sigma_1, printDecimalDegree)
-sigma_1 = handler.getPositveAngle(sigma_1)
-handler.printAngle('sigma_1', sigma_1, printDecimalDegree)
-
-# plus or minus ?? took minus for now
-lambda_1 = lambda_bar - 0.5 * sigma_1
-handler.printAngle('lambda_1', lambda_1, printDecimalDegree)
-
-################# END 1st step #################
-
-################# START 2nd step #################
-# apply half the slow equation to the computed result
-lambda_mu = longitude_slow_apogee
-handler.printAngle('lambda_mu', lambda_mu, printDecimalDegree)
-
-kappa_mu_1 = lambda_1 - lambda_mu
-handler.printAngle('kappa_mu_1', kappa_mu_1, printDecimalDegree)
-kappa_mu_1 = handler.getPositveAngle(kappa_mu_1)
-handler.printAngle('kappa_mu_1', kappa_mu_1, printDecimalDegree)
-
-# get the current size of the epicycle
-sizeSlow = getSizeEpicycle(sizeSlow_at_0, sizeSlow_at_90, radiusDeferent, handler, kappa_mu_1)
-#print('sizeSlow: \t' + str(sizeSlow))
-# convert the size to the radius
-radiusSlow = sizeSlow / 360. * radiusDeferent
-#print('radiusSlow: \t' + str(radiusSlow))
-
-mu_1 = getSlowEquation(radiusSlow, radiusDeferent, handler, kappa_mu_1)
-handler.printAngle('mu_1', mu_1, printDecimalDegree)
-mu_1 = handler.getPositveAngle(mu_1)
-handler.printAngle('mu_1', mu_1, printDecimalDegree)
-
-# plus or minus ?? took plus for now
-lambda_2 = lambda_1 + 0.5 * mu_1
-handler.printAngle('lambda_2', lambda_2, printDecimalDegree)
-
-################# END 2nd step #################
-
-################# START 3rd step #################
-# start form the computed result, compute the slow equation,
-# apply it whole to the mean planet
-kappa_mu_2 = lambda_2 - lambda_mu
-handler.printAngle('kappa_mu_2', kappa_mu_2, printDecimalDegree)
-kappa_mu_2 = handler.getPositveAngle(kappa_mu_2)
-handler.printAngle('kappa_mu_2', kappa_mu_2, printDecimalDegree)
-
-# get the current size of the epicycle
-sizeSlow = getSizeEpicycle(sizeSlow_at_0, sizeSlow_at_90, radiusDeferent, handler, kappa_mu_2)
-#print('sizeSlow: \t' + str(sizeSlow))
-# convert the size to the radius
-radiusSlow = sizeSlow / 360. * radiusDeferent
-#print('radiusSlow: \t' + str(radiusSlow))
-
-mu_2 = getSlowEquation(radiusSlow, radiusDeferent, handler, kappa_mu_2)
-handler.printAngle('mu_2', mu_2, printDecimalDegree)
-mu_2 = handler.getPositveAngle(mu_2)
-handler.printAngle('mu_2', mu_2, printDecimalDegree)
-
-# plus or minus ?? took plus for now
-lambda_3 = lambda_bar + mu_2
-handler.printAngle('lambda_3', lambda_3, printDecimalDegree)
-
-################# END 3rd step #################
-
-################# START 4th step #################
-# apply the whole fast equation to the computed result
-kappa_sigma_2 = lambda_3 - lambda_sigma
-handler.printAngle('kappa_sigma_2', kappa_sigma_2, printDecimalDegree)
-kappa_sigma_2 = handler.getPositveAngle(kappa_sigma_2)
-handler.printAngle('kappa_sigma_2', kappa_sigma_2, printDecimalDegree)
-
-# get the current size of the epicycle
-sizeFast = getSizeEpicycle(sizeFast_at_0, sizeFast_at_90, radiusDeferent, handler, kappa_sigma_2)
-#print('sizeFast: \t' + str(sizeFast))
-# convert the size to the radius
-radiusFast = sizeFast / 360. * radiusDeferent
-#print('radiusFast: \t' + str(radiusFast))
-
-sigma_2 = getFastEquation(radiusFast, radiusDeferent, handler, kappa_sigma_2)
-handler.printAngle('sigma_2', sigma_2, printDecimalDegree)
-sigma_2 = handler.getPositveAngle(sigma_2)
-handler.printAngle('sigma_2', sigma_2, printDecimalDegree)
-
-# plus or minus ?? took minus for now
-lambda_true = lambda_3 - sigma_2
-handler.printAngle('lambda_true', lambda_true, printDecimalDegree)
-lambda_true = handler.getPositveAngle(lambda_true)
-handler.printAngle('sigma_2', lambda_true, printDecimalDegree)
-
-################# END 4th step #################
+# time constants
+# deferent radius and sin handler
+# planet constants
+# epicycle radii
+# settings
+# the signs for the last calculation in each step
+for i in [-1, 1]:
+    for j in [-1, 1]:
+        for k in [-1, 1]:
+            for l in [-1, 1]:
+                print("####################################### " '{},{},{},{}'.format(i,j,k,l))
+                do4stepProcedure( \
+                    yuga, days_in_yuga, days_since_epoch, \
+                    radiusDeferent, handler, \
+                    meanPlanet_revolutions, fast_apogee_revolutions, longitude_slow_apogee, \
+                    sizeSlow_at_0, sizeSlow_at_90, sizeFast_at_0, sizeFast_at_90, \
+                    doRounding, printDecimalDegree, \
+                    i, j, k, l )
